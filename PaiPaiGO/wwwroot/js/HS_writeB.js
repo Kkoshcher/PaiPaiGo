07a53f5001021b5ec846f3214519339f2f0c5b4c
Kkoshcher <tn36m29rc1a7@gmail.com>
2023-10-20 22:26:02 +0800

    合併完成

--- PaiPaiGO/wwwroot/js/HS_writeB.js
+++ PaiPaiGO/wwwroot/js/HS_writeB.js
@@ -1,26 +1,15 @@
-﻿document.addEventListener("DOMContentLoaded", function () {
-    const inputs = document.querySelectorAll('input[type="text"], textarea');
-
-    inputs.forEach(input => {
-        const label = input.previousElementSibling;
-        const icon = document.createElement('span');
-        icon.className = 'iconicfill-pen-alt2';
-        icon.innerHTML = '&#9998;';
-        input.parentNode.insertBefore(icon, input.nextSibling);
-
-        input.addEventListener('focus', function () {
-            label.style.left = '100px';
-            icon.classList.remove('hide');
-            icon.classList.add('show');
+﻿$(document).ready(function () {
+    $(".wrap-label input[type='text'], .wrap-label textarea").focusin(function () {
+        $(this).siblings('label').animate({ left: '80px' }, 500);
+        $(this).siblings('.iconicfill-pen-alt2').animate({ left: '15px', opacity: 1 }, 500, function () {
+            $(this).addClass('move-pen');
         });
+    });
 
-        input.addEventListener('blur', function () {
-            label.style.left = '10px';
-            icon.classList.remove('show');
-            icon.classList.add('hide');
-            setTimeout(() => {
-                icon.classList.remove('hide');
-            }, 1000);
+    $(".wrap-label input[type='text'], .wrap-label textarea").focusout(function () {
+        $(this).siblings('label').animate({ left: '10px' }, 500);
+        $(this).siblings('.iconicfill-pen-alt2').animate({ left: '-30px', opacity: 0 }, 500, function () {
+            $(this).removeClass('move-pen');
         });
     });
 });
